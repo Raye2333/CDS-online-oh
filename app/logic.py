003303a2ccb9a20322aaee31bfdf0bfc11799931
Rishabh  Sarup <rs868@cornell.edu>
2020-03-30 01:13:15 -0400

    added imports and routes for obtaining queue and user in logic.py

--- app/logic.py
+++ app/logic.py
@@ -1,8 +1,17 @@
 import sqlite3
+import db 
+from models import Request, User
+import json 
+from flask import Flask, request
+import datetime
+
 
 def get_queue(course_id):
-  # help
-  pass
+  queue = Request.query.filter_by(course_id = course_id)
+  if not queue: 
+        return json.dumps({'success': False, 'error':'Queue for course not found!'}), 404
+  return json.dumps({'success': True, 'data': queue}), 200 
+
 
 def add_to_queue(queue_id, user_id):
   pass
@@ -12,4 +21,6 @@ def remove_from_queue(queue_id, user_id):
 
 def get_user(net_id):
     user = User.query.filter_by(net_id=net_id)
-    return user
+    if not user: 
+        return json.dumps({'success': False, 'error':'User not found!'}), 404
+    return json.dumps({'success': True, 'data': user}), 200 
